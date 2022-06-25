498f6fea063fe533aac993106f3da4759c4f0148
Leon Susanto <leonsusanto38@gmail.com>
2022-06-25 16:03:14 +0700

    json result

--- Simple Calculator Services/celerytask/task/views.py
+++ Simple Calculator Services/celerytask/task/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import redirect
 
 from .task import get_prime, get_prime_palindrom
@@ -6,12 +7,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def create_response(result):
+    response_data = {
+        'result': result
+    }
+    return json.dumps(response_data)
+
 def prime(request, n):
     prime = get_prime(n = n)
-    html = '<html><h1> Prime %s. </h1></html>'  %prime
-    return HttpResponse(html)
+    response = create_response(prime)
+    return HttpResponse(response, content_type="application/json")
 
 def palindrome(request, n):
     palindrome = get_prime_palindrom(n = n)
-    html = '<html><h1> Prime and Palindrome %s. </h1></html>' %palindrome
-    return HttpResponse(html)
+    response = create_response(palindrome)
+    return HttpResponse(response, content_type="application/json")
